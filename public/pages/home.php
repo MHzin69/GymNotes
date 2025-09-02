98980d8555bb123098ba0cb1b3ead61133b63519
MHzin69 <lohannluna@gmail.com>
2025-09-01 22:27:03 -0300

    banner concluido

--- public/pages/home.php
+++ public/pages/home.php
@@ -1,3 +1,7 @@
-    <div class="bannerIndex mx-auto text-center">
-        <h2>Organize seus treinos, evolua seus resultados</h3>
+    <div class="bannerIndex mx-auto ">
+        <h2>Organize seus treinos, evolua seus resultados</h2>
+        <div>
+            <a href="" class="btn btn-lg btn-primary">Comece agora</a>
+            <a href="" class="btn btn-lg btn-primary">Ver planilhas</a>
+        </div>
     </div>
