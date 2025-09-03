fe0302d0c58f42d1caa925f90105cd9b30b977c4
MHzin69 <lohannluna@gmail.com>
2025-09-03 14:15:23 -0300

    concluida a adaptaçao para a estrutura mvc

--- public/index.php
+++ public/index.php
@@ -1,54 +1,7 @@
 <!DOCTYPE html>
 <html lang="pt-br">
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Gym Notes</title>
 
-    <link rel="stylesheet" href="scss/style.css">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css"
-        integrity="sha512-... (truncado)" crossorigin="anonymous" referrerpolicy="no-referrer" />
-
-</head>
-
-<body>
-    <header>
-        <nav class="navbar navbar-expand-lg">
-            <div class="container-fluid">
-                <!-- Nome e Logo do site -->
-                <a class="navbar-brand" href="#"><img class="logoNav" src="img/logo/logoGymNotes.png" alt="Gym Notes" title="Home"></a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
-                    data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
-                    aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-                <!-- Itens do NAV -->
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
-                        <li class="nav-item">
-                            <a class="nav-link" aria-current="page" href="#">Início</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">Planilhas</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">Exercícios</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">Contato</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#"><i class="fa-solid fa-user"></i>Login</a>
-                        </li>
-
-                    </ul>
-                </div>
-            </div>
-        </nav>
-    </header>
     
     <main>
     <?php
@@ -65,10 +18,7 @@
     </main>
 
 
-    <!-- Script do Bootstrap -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-ENjdO4Dr2bkBIFxQpeoA6VKK5RZ9GF2z4l6Jv5l6b6p6g6l6b6p6g6l6b6p6g6l6"
-        crossorigin="anonymous"></script>
+    
 </body>
 
 </html>
